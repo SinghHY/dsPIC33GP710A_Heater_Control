0a6373b1e8d62b798613d84a4147b11ca76320e2
Harpreet Singh <Harpreet.Singh@perkinelmer.com>
2018-02-22 16:10:24 -0500

    Project Added with P only

---
+++ Heater_Control_710A.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Heater_Control_710A.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Heater_Control_710A.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=heatercontrol710a.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/heatercontrol710a.x.tar
